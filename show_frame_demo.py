60ff252033a2c9a8022c7f70a13c824c40d605ff
LR0 <18260056678@163.com>
2019-09-27 17:28:30 +0800

    no sense

--- show_frame_demo.py
+++ show_frame_demo.py
@@ -10,7 +10,7 @@ save_image_path = 'C:/Users/admin/Videos/cases/output/case7_images'
 
 
 start = datetime.datetime.now()
-save_biggest_emotion_images_cut(test_video_path, save_image_path, EmotionDetector(), 1000, 0, 10*60*1000)
+save_biggest_emotion_images(test_video_path, save_image_path, EmotionDetector(), 1000)
 end = datetime.datetime.now()
 print(end - start)
 
