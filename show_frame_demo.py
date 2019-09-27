94abe791fc9252f9a5cdce929beefd000184be64
LR0 <18260056678@163.com>
2019-09-27 17:17:05 +0800

    no sense

--- show_frame_demo.py
+++ show_frame_demo.py
@@ -5,8 +5,8 @@ from emotion_detector import EmotionDetector
 from utils.datasets import get_labels
 
 emotion_labels = get_labels('fer2013')
-test_video_path = 'C:/Users/admin/Videos/cases/case6.mp4'
-save_image_path = 'C:/Users/admin/Videos/cases/output/case6_images'
+test_video_path = 'C:/Users/admin/Videos/cases/case7.mp4'
+save_image_path = 'C:/Users/admin/Videos/cases/output/case7_images'
 
 
 start = datetime.datetime.now()
